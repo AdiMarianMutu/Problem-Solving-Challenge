f52f91197245f82b598052f2a951168b5e124272
Mutu Adi-Marian <39502043+Xxshark888xX@users.noreply.github.com>
2018-09-07 15:23:31 +0300

    Update method.cs

--- Utopian Tree/method.cs
+++ Utopian Tree/method.cs
@@ -4,13 +4,17 @@ static int utopianTree(int n) {
         return 1;
         
     int h = 1;
+    
+    // Much better like this
+    for (int i = 0; i < n; i++)
+	    h = (i % 2) == 0 ? h * 2 : h + 1;
 
-    for (int i = 0; i < n; i++) {
+    /*for (int i = 0; i < n; i++) {
         if ((i % 2) == 0)
             h *= 2;
         else 
             h++;
-    }
+    }*/
         
     return h;
 }
