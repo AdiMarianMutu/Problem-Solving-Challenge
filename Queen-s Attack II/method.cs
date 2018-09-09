ebc5fd40a3edbfb37feeb5e6ebf67310c57de261
Mutu Adi-Marian <39502043+Xxshark888xX@users.noreply.github.com>
2018-09-09 23:39:44 +0300

    Update method.cs

--- Queen-s Attack II/method.cs
+++ Queen-s Attack II/method.cs
@@ -1,52 +1,60 @@
-static int CalculatePosition(int x, int y, int k) {
-    // k  = board size
-    // x  = row
-    // x1 = current row => k - x
-    // y  = column
-    // Formula below:
-    // (((x + y) + (k * x1)) - k) + x1
-    int x1 = k - x;
-    return (((x + y) + (k * x1)) - k) + x1;
-}
-    
-static int[] GetObstaclePosition(int[][] piece, int k) {
-    int[] r = new int[piece.Length];
-
-    for (int i = 0; i < r.Length; i++)
-        r[i] = CalculatePosition(piece[i][0], piece[i][1], k);
+public class Chessboard {
+    int[][] obst;
         
-    return r.OrderBy(i => i).ToArray();
-}
-    
-static bool ObstacleExists(int[] obst, int pos) {
-    if (obst.Length == 0)
-        return false;
-        
-    int n = obst.Length / 2;
-        
-    if (pos == obst[n])
-        return true;
-                
-    if (pos > obst[n]) {
-        for (int i = n; i < obst.Length; i++) {
-            if (obst[i] == pos)
-                return true;
-        }
-    } else {
-        for (int i = n; i > 0; i--) {
-            if (obst[i] == pos)
-                return true;
-        }
+    public Chessboard(int[][] obst) {
+        this.obst = obst;
     }
         
-    return false;
+    public bool ObstacleExists(int r, int c) {
+        if (obst.Length == 0)
+            return false;
+
+        // To make the research of the obstacles on the chessboard
+        // We start to loop the array from his half
+        int n = obst.Length / 2;
+
+        // Sometimes the obstacle can be exactly at half of the array
+        // So if this happens and the current queen position is the same as the obstacle
+        // We return immediately true without searching the whole the array
+        if (obst[n][0] == r && obst[n][1] == c)
+            return true;
+
+        // Otherwise if the current queen row is above the value picked from the array
+        // We search the array starting from his half to his maximum length (to right)
+        if (r > obst[n][0]) {
+            for (int i = n; i < obst.Length; i++) {
+                if (obst[i][0] == r && obst[i][1] == c)
+                    return true;
+            }
+        // Same thing but from his half to 0 (to left)
+        } else if (r < obst[n][0]) {
+            for (int i = n; i >= 0; i--) {
+                if (obst[i][0] == r && obst[i][1] == c)
+                    return true;
+            }
+        // If the current queen row and the current array value are the same
+        // We search the array based on the column, using exactly the same logic used for the rows
+        } else {
+            if (c > obst[n][1]) {
+                for (int i = n; i < obst.Length; i++) {
+                    if (obst[i][0] == r && obst[i][1] == c)
+                        return true;
+                }
+            } else {
+                for (int i = n; i >= 0; i--) {
+                    if (obst[i][0] == r && obst[i][1] == c)
+                        return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
     
 static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
-    int[] obsPos    = GetObstaclePosition(obstacles, n);
+    Chessboard board = new Chessboard(obstacles.OrderBy(x => x[0]).ToArray());
         
-    int  qRow       = r_q;
-    int  qColumn    = c_q;
     int  qAttacks   = 0;
         
     bool qSouth     = true;
@@ -61,76 +69,76 @@ static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
     for (int i = 1; i <= n; i++) {
         // First we check if the queen isn't on the board edges
         // South direction
-        if (qSouth && (qRow - i) == 0)
+        if (qSouth && (r_q - i) == 0)
             qSouth = false;
         // Southeast direction
-        if (qSoutheast && (qRow - i == 0 || qColumn + i == n + 1))
+        if (qSoutheast && (r_q - i == 0 || c_q + i == n + 1))
             qSoutheast = false;
         // East direction
-        if (qEast && (qColumn + i) == n + 1)
+        if (qEast && (c_q + i) == n + 1)
             qEast = false;
         // Northeast direction
-        if (qNortheast && (qRow + i == n + 1 || qColumn + i == n + 1))
+        if (qNortheast && (r_q + i == n + 1 || c_q + i == n + 1))
             qNortheast = false;
         // North direction
-        if (qNorth && (qRow + i) == n + 1)
+        if (qNorth && (r_q + i) == n + 1)
             qNorth = false;
         // Northwest direction
-        if (qNorthwest && (qRow + i == n + 1 || qColumn - i == 0))
+        if (qNorthwest && (r_q + i == n + 1 || c_q - i == 0))
             qNorthwest = false;
         // West direction
-        if (qWest && (qColumn - i) == 0)
+        if (qWest && (c_q - i) == 0)
             qWest = false;
         // Southwest direction
-        if (qSouthwest && (qRow - i == 0 || qColumn - i == 0))
+        if (qSouthwest && (r_q - i == 0 || c_q - i == 0))
             qSouthwest = false;
             
         // Now we can check for the obstacles
         // Verify South way
         if (qSouth)
-            if (!ObstacleExists(obsPos, CalculatePosition(qRow - i, qColumn, n)))
+            if (!board.ObstacleExists(r_q - i, c_q))
                 qAttacks++;
             else
                 qSouth = false;
         // Verify Southeast way
         if (qSoutheast)
-            if (!ObstacleExists(obsPos, CalculatePosition(qRow - i, qColumn + i, n)))
+            if (!board.ObstacleExists(r_q - i, c_q + i))
                 qAttacks++;
             else
                 qSoutheast = false;
         // Verify East way
         if (qEast)
-            if (!ObstacleExists(obsPos, CalculatePosition(qRow, qColumn + i, n)))
+            if (!board.ObstacleExists(r_q, c_q + i))
                 qAttacks++;
             else
                 qEast = false;
         // Verify Northeast
         if (qNortheast)
-            if (!ObstacleExists(obsPos, CalculatePosition(qRow + i, qColumn + i, n)))
+            if (!board.ObstacleExists(r_q + i, c_q + i))
                 qAttacks++;
             else
                 qNortheast = false;
         // Verify North way
         if (qNorth)
-            if (!ObstacleExists(obsPos, CalculatePosition(qRow + i, qColumn, n)))
+            if (!board.ObstacleExists(r_q + i, c_q))
                 qAttacks++;
             else
                 qNorth = false;
         // Verify Northwest way
         if (qNorthwest)
-            if (!ObstacleExists(obsPos, CalculatePosition(qRow + i, qColumn - i, n)))
+            if (!board.ObstacleExists(r_q + i, c_q - i))
                 qAttacks++;
             else
                 qNorthwest = false;
         // Verify West way
         if (qWest)
-            if (!ObstacleExists(obsPos, CalculatePosition(qRow, qColumn - i, n)))
+            if (!board.ObstacleExists(r_q, c_q - i))
                 qAttacks++;
             else
                 qWest = false;
         // Verify Southwest way
         if (qSouthwest)
-            if (!ObstacleExists(obsPos, CalculatePosition(qRow - i, qColumn - i, n)))
+            if (!board.ObstacleExists(r_q - i, c_q - i))
                 qAttacks++;
             else
                 qSouthwest = false;
