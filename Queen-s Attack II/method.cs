29c4f10717c514fc9604fa09610e5878ad11adf7
Mutu Adi-Marian <39502043+Xxshark888xX@users.noreply.github.com>
2018-09-10 22:31:01 +0300

    Update method.cs

--- Queen-s Attack II/method.cs
+++ Queen-s Attack II/method.cs
@@ -1,10 +1,10 @@
 public class Chessboard {
-    int[] Obstacles;
-    int   Size;
+    HashSet<int> Obstacles = new HashSet<int>();
+    int          Size;
 
     public Chessboard(int[][] obstacles, int size) {
         this.Size      = size;
-        this.Obstacles = CalculateObstaclePositionTo1DArray(obstacles);
+        CalculateObstaclePosition(obstacles);
     }
 
     private int CalculatePosition(int r, int c) {
@@ -19,123 +19,116 @@ public class Chessboard {
         return (((r + c) + (n * x)) - n) + x;
     }
 
-    private int[] CalculateObstaclePositionTo1DArray(int[][] obst) {
-        int[] r = new int[obst.Length];
-
+    private void CalculateObstaclePosition(int[][] obst) {
         for (int i = 0; i < obst.Length; i++)
-            r[i] = CalculatePosition(obst[i][0], obst[i][1]);
-
-        return r;
+            Obstacles.Add(CalculatePosition(obst[i][0], obst[i][1]));
     }
 
     public bool ObstacleExists(int r, int c) {
-        if (Obstacles.Length == 0)
+        if (Obstacles.Count == 0)
             return false;
-                
-        int qPos = CalculatePosition(r, c);
 
-        for (int i = 0; i < Obstacles.Length; i++) {
-            if (qPos == Obstacles[i])
-                return true;
-        }
-
-        return false;
+        return Obstacles.Contains(CalculatePosition(r, c));
     }
 }
 
 static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles) {
     Chessboard board = new Chessboard(obstacles, n);
             
-    int  qAttacks   = 0;
-    bool qSouth     = true;
-    bool qSoutheast = true;
-    bool qEast      = true;
-    bool qNortheast = true;
-    bool qNorth     = true;
-    bool qNorthwest = true;
-    bool qWest      = true;
-    bool qSouthwest = true;
+    bool qN_Dir  = true;
+    bool qNW_Dir = true;
+    bool qNE_Dir = true;
+    bool qS_Dir  = true;
+    bool qSW_Dir = true;
+    bool qSE_Dir = true;
+    bool qE_Dir  = true;
+    bool qW_Dir  = true;
     
     for (int i = 1; i <= n; i++) {
-        // First we check if the queen isn't on the board edges
-        // South direction
-        if (qSouth && (r_q - i) == 0) 
-            qSouth = false;
-        // Southeast direction
-        if (qSoutheast && (r_q - i == 0 || c_q + i == n + 1))
-            qSoutheast = false;
-        // East direction
-        if (qEast && (c_q + i) == n + 1)
-            qEast = false;
-        // Northeast direction
-        if (qNortheast && (r_q + i == n + 1 || c_q + i == n + 1))
-            qNortheast = false;
-        // North direction
-        if (qNorth && (r_q + i) == n + 1)
-            qNorth = false;
-        // Northwest direction
-        if (qNorthwest && (r_q + i == n + 1 || c_q - i == 0))
-            qNorthwest = false;
-        // West direction
-        if (qWest && (c_q - i) == 0)
-            qWest = false;
-        // Southwest direction
-        if (qSouthwest && (r_q - i == 0 || c_q - i == 0))
-            qSouthwest = false;
-
-        // Now we can check for the obstacles
-        // Verify South way
-        if (qSouth)
-            if (!board.ObstacleExists(r_q - i, c_q))
-                qAttacks++;
-            else
-                qSouth = false;
-        // Verify Southeast way
-        if (qSoutheast)
-            if (!board.ObstacleExists(r_q - i, c_q + i))
-                qAttacks++;
-            else
-                qSoutheast = false;
-        // Verify East way
-        if (qEast)
-            if (!board.ObstacleExists(r_q, c_q + i))
-                qAttacks++;
-            else
-                qEast = false;
-        // Verify Northeast
-        if (qNortheast)
-            if (!board.ObstacleExists(r_q + i, c_q + i))
-                qAttacks++;
-            else
-                qNortheast = false;
-        // Verify North way
-        if (qNorth)
-            if (!board.ObstacleExists(r_q + i, c_q))
-                qAttacks++;
-            else
-                qNorth = false;
-        // Verify Northwest way
-        if (qNorthwest)
-            if (!board.ObstacleExists(r_q + i, c_q - i))
-                qAttacks++;
-            else
-                qNorthwest = false;
-        // Verify West way
-        if (qWest)
-            if (!board.ObstacleExists(r_q, c_q - i))
-                qAttacks++;
-            else
-                qWest = false;
-        // Verify Southwest way
-        if (qSouthwest)
-            if (!board.ObstacleExists(r_q - i, c_q - i))
-                qAttacks++;
-            else
-                qSouthwest = false;
-
-        // If the queen can't move anymore we break the for loop
-        if (!qSouth && !qSoutheast && !qEast && !qNortheast && !qNorth && !qNorthwest && !qWest && !qSouthwest)
+        // If the queen can't move anymore we can break the loop
+        if (!qN_Dir && !qNW_Dir && !qNE_Dir && !qS_Dir && !qSW_Dir && !qSE_Dir && !qE_Dir && !qW_Dir)
             break;
+
+        // North
+        if (qN_Dir) {
+            if ((r_q + i) != n + 1) {
+                if (!board.ObstacleExists(r_q + i, c_q))
+                    qAttacks++;
+                else
+                    qN_Dir = false;
+            } else
+                qN_Dir = false;
+         }
+         // Northeast
+         if (qNE_Dir) {
+            if (((r_q + i) != n + 1) && ((c_q + i) != n + 1)) {
+                if (!board.ObstacleExists(r_q + i, c_q + i))
+                    qAttacks++;
+                else
+                    qNE_Dir = false;
+            } else
+                qNE_Dir = false;
+         }
+         // East
+         if (qE_Dir) {
+            if ((c_q + i) != n + 1) {
+                if (!board.ObstacleExists(r_q, c_q + i))
+                    qAttacks++;
+                else
+                    qE_Dir = false;
+            } else
+                qE_Dir = false;
+         }
+         // Southeast
+         if (qSE_Dir) {
+            if (((r_q - i) != 0) && ((c_q + i) != n + 1)) {
+                if (!board.ObstacleExists(r_q - i, c_q + i))
+                    qAttacks++;
+                else
+                    qSE_Dir = false;
+            } else
+                qSE_Dir = false;
+         }
+         // South
+         if (qS_Dir) {
+            if ((r_q - i) != 0) {
+                if (!board.ObstacleExists(r_q - i, c_q))
+                    qAttacks++;
+                else
+                    qS_Dir = false;
+            } else
+                qS_Dir = false;
+         }
+         // Southwest
+         if (qSW_Dir) {
+            if (((r_q - i) != 0) && ((c_q - i) != 0)) {
+                if (!board.ObstacleExists(r_q - i, c_q - i))
+                    qAttacks++;
+                else
+                    qSW_Dir = false;
+            } else
+                        qSW_Dir = false;
+         }
+         // West
+         if (qW_Dir) {
+            if ((c_q - i) != 0) {
+                if (!board.ObstacleExists(r_q, c_q - i))
+                    qAttacks++;
+                else
+                    qW_Dir = false;
+            } else
+                qW_Dir = false;
+         }
+         // Northwest
+         if (qNW_Dir) {
+            if (((r_q + i) != n + 1) && ((c_q - i) != 0)) {
+                if (!board.ObstacleExists(r_q + i, c_q - i))
+                    qAttacks++;
+                else
+                    qNW_Dir = false;
+            } else
+                qNW_Dir = false;
+         }
     }
 
     return qAttacks;
