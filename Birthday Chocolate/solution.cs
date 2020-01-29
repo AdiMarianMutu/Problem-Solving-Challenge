71dac80822e256614a42ff220dede09ce00dad66
Mutu Adi-Marian <39502043+Xxshark888xX@users.noreply.github.com>
2020-01-29 21:23:53 +0200

    Update solution.cs

--- Birthday Chocolate/solution.cs
+++ Birthday Chocolate/solution.cs
@@ -2,7 +2,7 @@ static int birthday(List<int> s, int d, int m) {
   int cnt = 0;
   
   for (int i = 0; i <= s.Count - m; i++)
-  cnt = (s.Skip(i).Take(m)).Sum() == d ? cnt + 1 : cnt;
+    cnt = (s.Skip(i).Take(m)).Sum() == d ? cnt + 1 : cnt;
   
   return cnt;
 }
